1c75ae1f762f5c850d862e11a8789c67d0eba1f3
Garrett Squire <garrettsquire@gmail.com>
2015-05-15 17:23:20 -0700

    implemented the skeleton for the post processing

--- src/lib.rs
+++ src/lib.rs
@@ -9,14 +9,44 @@ use flate2::Compression;
 use flate2::write::GzEncoder;
 
 use iron::{AfterMiddleware, IronResult, IronError, Request, Response};
+use iron::headers::{ContentEncoding, Encoding};
+
+#[derive(Clone)]
+pub enum Type {
+    Deflate,
+    Gzip
+}
 
 /// The EncoderWriter is used from the flate2 crate.
 pub struct Compressor {
-    engine: GzEncoder<Vec<u8>>
+    engine: Type
 }
 
 impl Compressor {
-    pub fn new() -> Compressor {
-        Compressor { engine: GzEncoder::new(Vec::new(), Compression::Best) }
+    pub fn new(compression: Type) -> Compressor {
+        Compressor { engine: compression }
+    }
+}
+
+impl AfterMiddleware for Compressor {
+    fn after(&self, _: &mut Request, mut res: Response) -> IronResult<(Response)> {
+        let mut ce_opts = Vec::new();
+
+        match self.engine {
+            Type::Deflate => {
+                ce_opts.push(Encoding::Deflate);
+                res.headers.set(ContentEncoding(ce_opts));
+            },
+            Type::Gzip => {
+                ce_opts.push(Encoding::Gzip);
+                res.headers.set(ContentEncoding(ce_opts));
+            }
+        }
+
+        Ok(res)
+    }
+
+    fn catch(&self, _: &mut Request, err: IronError) -> IronResult<Response> {
+        Err(err)
     }
 }
