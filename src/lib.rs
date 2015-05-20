1cbb5ee9a8e6bd9b52d71f1ada2fab4a7a1add80
Garrett Squire <garrettsquire@gmail.com>
2015-05-20 15:15:30 -0700

    still trying to figure out the proper types

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,13 @@
 extern crate flate2;
 extern crate iron;
 
-use std::io::prelude::*;
+use std::io::Read;
 
 use flate2::Compression;
-use flate2::write::GzEncoder;
+use flate2::read::{DeflateEncoder, GzEncoder};
 
 use iron::{AfterMiddleware, IronResult, IronError, Request, Response};
-use iron::headers::{ContentEncoding, Encoding};
+use iron::headers::{AcceptEncoding, ContentEncoding, Encoding};
 
 #[derive(Clone)]
 pub enum Type {
@@ -29,17 +29,23 @@ impl Compressor {
 }
 
 impl AfterMiddleware for Compressor {
-    fn after(&self, _: &mut Request, mut res: Response) -> IronResult<(Response)> {
+    fn after(&self, req: &mut Request, mut res: Response) -> IronResult<(Response)> {
+        if !req.headers.has::<AcceptEncoding>() {
+            return Ok(res);
+        }
+
         let mut ce_opts = Vec::new();
 
         match self.engine {
             Type::Deflate => {
                 ce_opts.push(Encoding::Deflate);
                 res.headers.set(ContentEncoding(ce_opts));
+                res.body = Some(Box::new(DeflateEncoder::new(res.body.unwrap(), Compression::Best)));
             },
             Type::Gzip => {
                 ce_opts.push(Encoding::Gzip);
                 res.headers.set(ContentEncoding(ce_opts));
+                res.body = Some(Box::new(GzEncoder::new(res.body.unwrap(), Compression::Best)));
             }
         }
 
