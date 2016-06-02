2982894f8580155f6a70f121269842ab697019db
Garrett Squire <garrettsquire@gmail.com>
2016-06-02 16:16:53 -0700

    change the after middleware to be a modifier instead

--- src/lib.rs
+++ src/lib.rs
@@ -1,82 +1,38 @@
-//! Compression middleware for Iron.
+//! Compression modifier for Iron.
 
 extern crate flate2;
 extern crate iron;
+extern crate modifier;
 
-use std::io::{Read, Write};
+use std::io::Write;
 
 use flate2::Compression;
 use flate2::write::{DeflateEncoder, GzEncoder};
 
-use iron::{AfterMiddleware, IronResult, IronError, Request, Response};
-use iron::headers::{AcceptEncoding, ContentEncoding, Encoding};
+use iron::Response;
+use iron::headers::{ContentEncoding, Encoding};
 
-#[derive(Clone, PartialEq, PartialOrd)]
-pub enum Type {
-    Deflate,
-    Gzip
-}
+use modifier::Modifier;
 
-/// The EncoderWriter is used from the flate2 crate.
-pub struct Compressor {
-    engine: Type
-}
+pub struct DeflateWriter<'a>(pub &'a [u8]);
+pub struct GzipWriter<'a>(pub &'a [u8]);
 
-impl Compressor {
-    pub fn new(compression: Type) -> Compressor {
-        Compressor { engine: compression }
+impl<'a> Modifier<Response> for DeflateWriter<'a> {
+    fn modify(self, res: &mut Response) {
+        res.headers.set(ContentEncoding(vec![Encoding::Deflate]));
+        let mut encoder = DeflateEncoder::new(Vec::new(), Compression::Best);
+        let _ = encoder.write_all(self.0);
+        let compressed_bytes = encoder.finish().unwrap();
+        compressed_bytes.modify(res);
     }
 }
 
-impl AfterMiddleware for Compressor {
-    fn after(&self, req: &mut Request, mut res: Response) -> IronResult<Response> {
-        let encodings = req.headers.get::<AcceptEncoding>();
-        let mut can_encode: bool = false;
-        match encodings {
-            None => { return Ok(res); }
-            Some(encodings) => {
-                for enc in encodings.iter() {
-                    if enc.item == Encoding::Gzip && self.engine == Type::Gzip
-                        || enc.item == Encoding::Deflate && self.engine == Type::Deflate {
-                            can_encode = true;
-                            break;
-                    }
-                }
-                if !can_encode {
-                    return Ok(res);
-                }
-            }
-        }
-
-        let mut ce_opts = Vec::new();
-        let mut req_body = Vec::new();
-        let _ = req.body.read_to_end(&mut req_body);
-
-        match self.engine {
-            Type::Deflate => {
-                ce_opts.push(Encoding::Deflate);
-                res.headers.set(ContentEncoding(ce_opts));
-
-                let mut def_enc = DeflateEncoder::new(vec![], Compression::Best);
-                let _ = def_enc.write(&req_body[..]);
-                let bytes = def_enc.finish().unwrap();
-                res.body = Some(Box::new(bytes));
-            },
-            Type::Gzip => {
-                ce_opts.push(Encoding::Gzip);
-                res.headers.set(ContentEncoding(ce_opts));
-
-                let mut gz_enc = GzEncoder::new(vec![], Compression::Best);
-                let _ = gz_enc.write(&req_body[..]);
-                let bytes = gz_enc.finish().unwrap();
-                res.body = Some(Box::new(bytes));
-            }
-        }
-
-        Ok(res)
-    }
-
-    fn catch(&self, _: &mut Request, err: IronError) -> IronResult<Response> {
-        Err(err)
+impl<'a> Modifier<Response> for GzipWriter<'a> {
+    fn modify(self, res: &mut Response) {
+        res.headers.set(ContentEncoding(vec![Encoding::Gzip]));
+        let mut encoder = GzEncoder::new(Vec::new(), Compression::Best);
+        let _ = encoder.write_all(self.0);
+        let compressed_bytes = encoder.finish().unwrap();
+        compressed_bytes.modify(res);
     }
 }
