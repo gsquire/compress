adf94687aba3689115851478b48d340a02ba5221
Garrett Squire <garrettsquire@gmail.com>
2016-06-03 10:25:04 -0700

    update documentation and README

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # compress
+This small library exposes two types that know how to encode bytes based on the GZIP and deflate
+algorithms. The types implement the modifier trait so they can be used in an Iron Response.
+
+### Example
+```rust
+extern crate iron;
+extern crate iron_compress;
+
+use iron::prelude::*;
+use iron::status;
+
+use iron_compress::GzipWriter;
+
+fn main() {
+    fn hello_world(_: &mut Request) -> IronResult<Response> {
+        Ok(Response::with((status::Ok, GzipWriter(b"Some compressed response"))))
+    }
+
+    Iron::new(hello_world).http("localhost:3000").unwrap();
+}
+```
+
+### License
+MIT
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-//! Compression modifier for Iron.
+/// Compression modifier for Iron. This simple crate lets you pass bytes to a response such
+/// that they can be encoded by either GZIP or deflate algorithms. The modifiers also set the
+/// proper content encoding header.
 
 extern crate flate2;
 extern crate iron;
@@ -14,10 +16,18 @@ use iron::headers::{ContentEncoding, Encoding};
 
 use modifier::Modifier;
 
+/// A type that knows how to encode a stream of bytes with the deflate algorithm.
 pub struct DeflateWriter<'a>(pub &'a [u8]);
+
+/// A type that knows how to encode a stream of bytes with the GZIP algorithm.
 pub struct GzipWriter<'a>(pub &'a [u8]);
 
 impl<'a> Modifier<Response> for DeflateWriter<'a> {
+    /// Encode the bytes with the deflate algorithm.
+    ///
+    /// # Panics
+    ///
+    /// This will panic if the encoder cannot finish writing the stream.
     fn modify(self, res: &mut Response) {
         res.headers.set(ContentEncoding(vec![Encoding::Deflate]));
         let mut encoder = DeflateEncoder::new(Vec::new(), Compression::Best);
@@ -28,6 +38,11 @@ impl<'a> Modifier<Response> for DeflateWriter<'a> {
 }
 
 impl<'a> Modifier<Response> for GzipWriter<'a> {
+    /// Encode the bytes with the GZIP algorithm.
+    ///
+    /// # Panics
+    ///
+    /// This will panic if the encoder cannot finish writing the stream.
     fn modify(self, res: &mut Response) {
         res.headers.set(ContentEncoding(vec![Encoding::Gzip]));
         let mut encoder = GzEncoder::new(Vec::new(), Compression::Best);
